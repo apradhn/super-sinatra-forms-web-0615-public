7a258cb9be8158004c2fe246e3f407c769a65b6c
apradhn <abhijit.pradhan6@gmail.com>
2015-07-01 14:35:32 -0400

    all done

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/' do 
+    erb :'super_hero'
+  end
+
+  get '/teams' do 
+    binding.pry
+    "hello"
+  end
+
+  post '/teams' do
+    @team = {movie: params[:team][:name], motto: params[:team][:motto]}
+
+    @heroes = params[:team][:members].collect{|hero, hash| hash}
+
+    erb :'/team'
+  end
 
 end
--- spec/super_sinatra_forms_spec.rb
+++ spec/super_sinatra_forms_spec.rb
@@ -43,6 +43,7 @@ describe App do
     end
 
     it 'sends a 200 status code' do 
+      # binding.pry
       expect(last_response.status).to eq(200)
     end
 
